e522840727d8b56b1c4a15343944ab3c31bcd993
pejuangvscode <151711997+pejuangvscode@users.noreply.github.com>
2025-09-04 23:20:45 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
-import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react()],
   publicDir: "./static",
   base: "/",
   css: {
